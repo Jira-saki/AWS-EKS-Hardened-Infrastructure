ea1c9de2077b979d1effa526130633bd630fdb86
Jira-saki <geepep@gmail.com>
2026-08-04 10:37:16 +0900

    feat(local-hob): complete terraform module refactoring and cloud-init configs

--- terraform/environments/local-hob/main.tf
+++ terraform/environments/local-hob/main.tf
@@ -6,7 +6,7 @@ module "network" {
 
 module "compute" {
   source          = "../../modules/compute"
-  pool_name = libvirt_pool.ep2_pool.name
+  pool_name       = libvirt_pool.ep2_pool.name
   dmz_net_id      = module.network.dmz_net_id
   isolated_net_id = module.network.isolated_net_id
 }
